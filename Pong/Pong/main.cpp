cd5153fb6460ad0ca8fc421f8185d7e916b59e1b
SLogic03 <jslojan@espe.edu.ec>
2024-03-04 16:54:04 -0500

    Cambio Variables y logica, MoverPelota, RenderizarVentana, Main

--- Pong/Pong/main.cpp
+++ Pong/Pong/main.cpp
@@ -2,15 +2,16 @@
 #include <SFML/Window.hpp>
 
 using namespace sf;
+using namespace std;
 
 //Declaracion Objetos Visuales
-RenderWindow(VideoMde(800, 600), "Pong");
+RenderWindow ventana(VideoMode(800, 600), "Pong");
 RectangleShape paleta1(Vector2f(10, 80));
 RectangleShape paleta2(Vector2f(10, 80));
 CircleShape pelota(5);
 
 //velocidad y direccion de pelota
-float velocidadPelota = o.45f;
+float velocidadPelota = 0.45f;
 Vector2f velocidadPelotaVector(velocidadPelota, velocidadPelota);
 
 //puntuacion de cada jugador
@@ -20,25 +21,33 @@ int puntajeJugador2 = 0;
 //Metodo Movimiento Paletas
 void moverPaletas()
 {
-    if (Keyboard::isKeyPressed(Keyboard::W) && paddle1.getPosition().y > 0)
+    if (Keyboard::isKeyPressed(Keyboard::W) && paleta1.getPosition().y > 0)
     {
         paleta1.move(0, -0.5);
     }
 
-    if (Keyboard::isKeyPressed(Keyboard::S) && paddle1.getPosition().y < 600 - paddle1.getSize().y)
+    if (Keyboard::isKeyPressed(Keyboard::S) && paleta1.getPosition().y < 600 - paleta1.getSize().y)
     {
         paleta1.move(0, 0.5);
     }
 
-    if (Keyboard::isKeyPressed(Keyboard::Up) && paddle2.getPosition().y > 0)
+    if (Keyboard::isKeyPressed(Keyboard::Up) && paleta2.getPosition().y > 0)
     {
         paleta2.move(0, -0.5);
     }
 
-    if (Keyboard::isKeyPressed(Keyboard::Down) && paddle2.getPosition().y < 600 - paddle2.getSize().y)
+    if (Keyboard::isKeyPressed(Keyboard::Down) && paleta2.getPosition().y < 600 - paleta2.getSize().y)
     {
         paleta2.move(0, 0.5);
     }
+}
+
+//Metodo Resetear Pelota
+void resetearPelota()
+{
+    pelota.setPosition(400,300);
+    velocidadPelotaVector.x = velocidadPelota;
+    velocidadPelotaVector.y = velocidadPelota;
 }
 
 //Metodo Movimiento Pelota
@@ -53,49 +62,86 @@ void moverPelota()
     }
 
     //Colision con paletas
-
-}
-
-//Metodo Resetear Pelota
-void resetBall()
-{
-    pelota.setPosition(400,300);
-    velocidadPelotaVector.x = velocidadPelota;
-    velocidadPelotaVector.y = velocidadPelota;
-}
-
-int main()
-{
-    // Create the main window
-    RenderWindow app(VideoMode(800, 600), "SFML window");
-
-    // Load a sprite to display
-    Texture texture;
-    if (!texture.loadFromFile("cb.bmp"))
-        return EXIT_FAILURE;
-    Sprite sprite(texture);
-
-    // Start the game loop
-    while (app.isOpen())
+    if ((pelota.getGlobalBounds().intersects(paleta1.getGlobalBounds())) ||
+    (pelota.getGlobalBounds().intersects(paleta2.getGlobalBounds())))
     {
-        // Process events
-        Event event;
-        while (app.pollEvent(event))
-        {
-            // Close window : exit
-            if (event.type == Event::Closed)
-                app.close();
-        }
-
-        // Clear screen
-        app.clear();
-
-        // Draw the sprite
-        app.draw(sprite);
-
-        // Update the window
-        app.display();
+        velocidadPelotaVector.x = -velocidadPelotaVector.x;
     }
 
-    return EXIT_SUCCESS;
+    //Puntuacion y reseteo de pelota
+    if (pelota.getPosition().x <= 0)
+    {
+        //Jugador 2
+        puntajeJugador2++;
+        resetearPelota();
+    }
+    if (pelota.getPosition().x >= 800 - pelota.getGlobalBounds().width)
+    {
+        //Jugador 1
+        puntajeJugador1++;
+        resetearPelota();
+    }
+}
+
+//Metodo Renderiza Ventada Visual
+void renderizarVentana()
+{
+    //Declaracion en Ventana
+    ventana.clear();
+    ventana.draw(paleta1);
+    ventana.draw(paleta2);
+    ventana.draw(pelota);
+
+    //Mostrar puntaje
+    Font fuente;
+    if (fuente.loadFromFile("Arial.ttf"))
+    {
+        Text textoPuntaje("Jugador 1: " + to_string(puntajeJugador1) + "   Jugador 2: " + to_string(puntajeJugador2), fuente, 20);
+        textoPuntaje.setPosition(250, 30);
+        ventana.draw(textoPuntaje);
+    }
+
+    ventana.display();
+}
+
+//Metodo Manejar Menu de Inicio
+int mostrarMenu()
+{
+    //Se encargara de mostrar MENU, donde cuente con dos opciones de 1 vs 1 y 1 vs BOT
+
+    //Implementar fuente ARIAL, archivo ya puesto en la carpeta del proyecto
+}
+
+//Metodo Principal(Ejecutar)
+int main()
+{
+    int eleccionMenu = mostrarMenu();
+
+    if (eleccionMenu == 1)
+    {
+        //Jugar 1 vs 1
+        paleta1.setPosition(50, 300);
+        paleta1.setPosition(750, 300);
+
+        while (ventana.isOpen())
+        {
+            Event evento;
+            while (ventana.pollEvent(evento))
+            {
+                if (evento.type == Event::Closed)
+                    ventana.close();
+            }
+
+            moverPaletas();
+            moverPelota();
+            renderizarVentana();
+        }
+    }
+    else if (eleccionMenu == 2)
+    {
+        //Jugar 1 vs BOT
+        //Implementar Logica con IA
+    }
+
+    return 0;
 }
